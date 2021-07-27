82336bb38d95e50e3672cbbc7c8e078f46c81b71
SAI GANESH DOKALA <61933709+saiganesh-d@users.noreply.github.com>
2021-07-28 00:30:22 +0530

    Update views.py

--- contents/views.py
+++ contents/views.py
@@ -2,9 +2,7 @@ from django.contrib.auth.models import User
 from django.http import FileResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from os.path import splitext
-from .models import (Certification, Education, Focus, ProfessionalSkill,
-                     Profile, Project, ProjectCategory, Recommendation,
-                     Seminar, TechnicalSkill, WorkExperience)
+
 
 
 
