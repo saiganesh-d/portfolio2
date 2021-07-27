fd85b10d950469650fdfa1abd4dfb41d1172b761
SAI GANESH DOKALA <61933709+saiganesh-d@users.noreply.github.com>
2021-07-28 00:27:55 +0530

    Update views.py

--- contents/views.py
+++ contents/views.py
@@ -7,23 +7,7 @@ from .models import (Certification, Education, Focus, ProfessionalSkill,
                      Seminar, TechnicalSkill, WorkExperience)
 
 
-def certification(request, pk):
-    file = get_object_or_404(Certification, pk=pk)
-    file_path = str(file.document)
-    file_type = ''
 
-    try:
-        # Get the file extension:
-        extension = splitext(file_path[16:])[1]
-        if extension == '.pdf':
-            file_type = 'application/pdf'
-
-        response = FileResponse(open(f'media/{file_path}', 'rb'), content_type=file_type)
-        response['Content-Disposition'] = f'filename={file_path[16:]}'
-
-        return response
-    except FileNotFoundError:
-        raise Http404()
 
 
 def home(request):
